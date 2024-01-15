8d18f5bf6872e9523324d231c6f3dbce93d5083f
Davide Bucciarelli <davidebuccioo@gmail.com>
2024-01-15 22:54:34 +0100

    created download_batch.py
    It downloads files from GDC in a batch fashion

---
+++ Data/download_batch.py
@@ -0,0 +1,59 @@
+import requests
+import json
+import re
+import os
+files_endpt = "https://api.gdc.cancer.gov/files"
+with open('Data/query.json') as json_file:  
+    filters = json.load(json_file)
+
+# Here a GET is used, so the filter parameters should be passed as a JSON string.
+
+params = {
+    "filters": json.dumps(filters),
+    "fields": "file_id",
+    "format": "JSON",
+    "size": "1000"
+    }
+
+response = requests.get(files_endpt, params = params)
+
+file_uuid_list = []
+batch_file_names = []
+all_file_names = []
+# This step populates the download list with the file_ids from the previous query
+for file_entry in json.loads(response.content.decode("utf-8"))["data"]["hits"]:
+    file_uuid_list.append(file_entry["file_id"])
+
+data_endpt = "https://api.gdc.cancer.gov/data"
+data_dir = 'wsis/'
+if not os.path.exists(data_dir):
+    os.makedirs(data_dir)
+params = {"ids": file_uuid_list}
+with open("Data/ids.json", "w") as output_file:
+    json.dump(params, output_file)
+ids = params['ids']
+batch_size = 10
+for i, id in enumerate(ids):
+    data_endpt = "https://api.gdc.cancer.gov/data/{}".format(id)
+    response = requests.get(data_endpt, headers = {"Content-Type": "application/json"})
+
+    # The file name can be found in the header within the Content-Disposition key.
+    response_head_cd = response.headers["Content-Disposition"]
+
+    file_name = os.path.join(data_dir, re.findall("filename=(.+)", response_head_cd)[0])
+    batch_file_names.append(file_name)
+    with open(file_name, "wb") as output_file:
+        output_file.write(response.content)
+    print(f"file {i} downloaded")
+    if i % batch_size == 0:
+        #DO THE PROCESSSING AND SAVE THE EMBEDDINGS HERE
+        #remove files in the file_names list
+        for file in batch_file_names:
+            os.remove(file)
+        all_file_names.extend(batch_file_names)
+        batch_file_names = []
+        print(f"batch {i} done")
+names = {}
+names['names'] = all_file_names
+with open("Data/names.json", "w") as output_file:
+    json.dump(names, output_file)
