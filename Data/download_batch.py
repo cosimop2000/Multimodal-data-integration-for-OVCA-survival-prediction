f69f0be9fc4235a3c50e5e334c443bd023fc5ddd
Bucci23 <davidebuccioo@gmail.com>
2024-01-16 11:15:17 +0100

    batch download updated
    Now also handles resuming an interrupted download

--- Data/download_batch.py
+++ Data/download_batch.py
@@ -5,35 +5,46 @@ import os
 files_endpt = "https://api.gdc.cancer.gov/files"
 with open('Data/query.json') as json_file:  
     filters = json.load(json_file)
+resume = True
+params = {}
+if not resume:
+    # Here a GET is used, so the filter parameters should be passed as a JSON string.
+    params = {
+        "filters": json.dumps(filters),
+        "fields": "file_id",
+        "format": "JSON",
+        "size": "1000"
+        }
 
-# Here a GET is used, so the filter parameters should be passed as a JSON string.
+    response = requests.get(files_endpt, params = params)
 
-params = {
-    "filters": json.dumps(filters),
-    "fields": "file_id",
-    "format": "JSON",
-    "size": "1000"
-    }
+    file_uuid_list = []
+    batch_file_names = []
+    all_file_names = []
+    # This step populates the download list with the file_ids from the previous query
+    for file_entry in json.loads(response.content.decode("utf-8"))["data"]["hits"]:
+        file_uuid_list.append(file_entry["file_id"])
 
-response = requests.get(files_endpt, params = params)
-
-file_uuid_list = []
-batch_file_names = []
-all_file_names = []
-# This step populates the download list with the file_ids from the previous query
-for file_entry in json.loads(response.content.decode("utf-8"))["data"]["hits"]:
-    file_uuid_list.append(file_entry["file_id"])
-
-data_endpt = "https://api.gdc.cancer.gov/data"
-data_dir = 'wsis/'
-if not os.path.exists(data_dir):
-    os.makedirs(data_dir)
-params = {"ids": file_uuid_list}
-with open("Data/ids.json", "w") as output_file:
-    json.dump(params, output_file)
+    data_endpt = "https://api.gdc.cancer.gov/data"
+    data_dir = 'wsis/'
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
+    params = {"ids": file_uuid_list, "iter":0}
+    with open("Data/ids.json", "w") as output_file:
+        json.dump(params, output_file)
+else:
+    with open("Data/ids.json", "r") as input_file:
+        params = json.load(input_file)
 ids = params['ids']
+start = params['iter']
 batch_size = 10
 for i, id in enumerate(ids):
+    if i < start:
+        continue
+    print(f'starting from file {i}')
+    params['iter'] = i
+    with open("Data/ids.json", "w") as output_file:
+        json.dump(params, output_file)
     data_endpt = "https://api.gdc.cancer.gov/data/{}".format(id)
     response = requests.get(data_endpt, headers = {"Content-Type": "application/json"})
 
